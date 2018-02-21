ff13cc196d2fc06a22596ebc636cc4943192741f
ergenekonyigit <yigitergenekon@gmail.com>
2018-02-21 07:18:09 +0300

    fix

--- src/module.js
+++ src/module.js
@@ -3,10 +3,9 @@ import log from './pretty-log'
 export const sum = (...arr) => [...arr].reduce((acc, val) => acc + val, 0);
 
 const x = [1, 2]
-const y = [3, 4]
 
-function sumArrays(x, y) {
-	return sum(...x) + sum(...y)
+function sumArrays(x) {
+	return sum(...x) + 5
 }
 
-log(sumArrays(x, y))
+log(sumArrays(x))
